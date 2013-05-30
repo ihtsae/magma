ihtsae/magma

commit e4ce8d9a9274a069755268748c969a8283f6b1ee

replace cu(Float|Double) with magma$1 (in magmablas)
git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@2886 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

magmablas/gemm_stencil_defs.cu
@@ -9,9 +9,9 @@
 
 #ifdef COMPLEX
   #ifdef DOUBLE
-    typedef cuDoubleComplex FloatingPoint_t;
+    typedef magmaDoubleComplex FloatingPoint_t;
   #else
-    typedef cuFloatComplex FloatingPoint_t;
+    typedef magmaFloatComplex FloatingPoint_t;
   #endif
 #else
   #ifdef DOUBLE
@@ -31,7 +31,7 @@
         FloatingPoint_t tex_fetch(texture<int4> tex_ref, int coord)
         {
             int4 v = tex1Dfetch(tex_ref, coord);
-            return make_cuDoubleComplex(__hiloint2double(v.y, v.x), __hiloint2double(v.w, v.z));
+            return make_magmaDoubleComplex(__hiloint2double(v.y, v.x), __hiloint2double(v.w, v.z));
         }
       #else
         static __device__
@@ -72,13 +72,13 @@
     #define add(A, B)        cuCadd(A, B)
     #define mul(A, B)        cuCmul(A, B)
     #define fma(A, B, C) C = cuCfma(A, B, C)
-    #define make_FloatingPoint(x, y) make_cuDoubleComplex(x, y);
+    #define make_FloatingPoint(x, y) make_magmaDoubleComplex(x, y);
   #else
     #define conj(A)          cuConjf(A)
     #define add(A, B)        cuCaddf(A, B)
     #define mul(A, B)        cuCmulf(A, B)
     #define fma(A, B, C) C = cuCfmaf(A, B, C)
-    #define make_FloatingPoint(x, y) make_cuFloatComplex(x, y);
+    #define make_FloatingPoint(x, y) make_magmaFloatComplex(x, y);
   #endif
 #else
     #define conj(A)           (A)

magmablas/zgemvt_fermi.cu
@@ -14,21 +14,21 @@
 #define magmablas_zgemvt_fermi magmablas_zgemvt
 
 __global__ void 
-zgemvtn_kernel2_fermi(int n, int m, int n1, cuDoubleComplex alpha,  cuDoubleComplex* A, int lda, cuDoubleComplex *x, int incx, cuDoubleComplex *y)
+zgemvtn_kernel2_fermi(int n, int m, int n1, magmaDoubleComplex alpha,  magmaDoubleComplex* A, int lda, magmaDoubleComplex *x, int incx, magmaDoubleComplex *y)
 {
   int ind = blockIdx.x*num_threads + threadIdx.x;
 
   A += ind;
   x += threadIdx.x*incx;
 
-  cuDoubleComplex res;
+  magmaDoubleComplex res;
   MAGMA_Z_SET2REAL(res, 0.0f);
 
-  __shared__ cuDoubleComplex buff[num_threads];
+  __shared__ magmaDoubleComplex buff[num_threads];
   for(int i=0; i<n1; i += num_threads ){
     __syncthreads();
     //buff[threadIdx.x]  = x[i];
-    buff[threadIdx.x]  = make_cuDoubleComplex (cuCreal(x[i*incx]), -cuCimag(x[i*incx]));
+    buff[threadIdx.x]  = make_magmaDoubleComplex (cuCreal(x[i*incx]), -cuCimag(x[i*incx]));
 
     __syncthreads();
     #pragma unroll
@@ -41,7 +41,7 @@ zgemvtn_kernel2_fermi(int n, int m, int n1, cuDoubleComplex alpha,  cuDoubleComp
 
   if (m>n1){
      //buff[threadIdx.x]  = x[n1];
-     buff[threadIdx.x]  = make_cuDoubleComplex (cuCreal(x[n1*incx]), -cuCimag(x[n1*incx]));
+     buff[threadIdx.x]  = make_magmaDoubleComplex (cuCreal(x[n1*incx]), -cuCimag(x[n1*incx]));
 
      __syncthreads();
      for(int j=0; j<(m-n1); j++){
@@ -55,7 +55,7 @@ zgemvtn_kernel2_fermi(int n, int m, int n1, cuDoubleComplex alpha,  cuDoubleComp
 }
 
 extern "C" void
-magmablas_zgemvtn_fermi(int n, int m, cuDoubleComplex alpha, cuDoubleComplex *A, int lda, cuDoubleComplex *x, int incx, cuDoubleComplex *y)
+magmablas_zgemvtn_fermi(int n, int m, magmaDoubleComplex alpha, magmaDoubleComplex *A, int lda, magmaDoubleComplex *x, int incx, magmaDoubleComplex *y)
 {
 /*  -- MAGMA (version 1.1) --
        Univ. of Tennessee, Knoxville
@@ -105,8 +105,8 @@ magmablas_zgemvtn_fermi(int n, int m, cuDoubleComplex alpha, cuDoubleComplex *A,
 
 
 extern "C" void
-magmablas_zgemvt_fermi(char flag, int m, int n, cuDoubleComplex alpha, 
-                       cuDoubleComplex *A, int lda, cuDoubleComplex *x, int incx, cuDoubleComplex beta, cuDoubleComplex *y, int incy) 
+magmablas_zgemvt_fermi(char flag, int m, int n, magmaDoubleComplex alpha, 
+                       magmaDoubleComplex *A, int lda, magmaDoubleComplex *x, int incx, magmaDoubleComplex beta, magmaDoubleComplex *y, int incy) 
 {
 
     //if(beta.x==0.0 && beta.y==0.0 && incx ==1 && incy==1)

commit 3a414317e6313b1eb92de637a54d2f481759542c

Fix complex, broken by r2886
git-svn-id: https://icl.cs.utk.edu/svn/magma/trunk@2897 02bb5f2a-90ca-11de-bb24-8aedcc835bc8

magmablas/gemm_stencil_defs.cu
@@ -31,7 +31,7 @@
         FloatingPoint_t tex_fetch(texture<int4> tex_ref, int coord)
         {
             int4 v = tex1Dfetch(tex_ref, coord);
-            return make_magmaDoubleComplex(__hiloint2double(v.y, v.x), __hiloint2double(v.w, v.z));
+            return make_cuDoubleComplex(__hiloint2double(v.y, v.x), __hiloint2double(v.w, v.z));
         }
       #else
         static __device__
@@ -72,13 +72,13 @@
     #define add(A, B)        cuCadd(A, B)
     #define mul(A, B)        cuCmul(A, B)
     #define fma(A, B, C) C = cuCfma(A, B, C)
-    #define make_FloatingPoint(x, y) make_magmaDoubleComplex(x, y);
+    #define make_FloatingPoint(x, y) make_cuDoubleComplex(x, y);
   #else
     #define conj(A)          cuConjf(A)
     #define add(A, B)        cuCaddf(A, B)
     #define mul(A, B)        cuCmulf(A, B)
     #define fma(A, B, C) C = cuCfmaf(A, B, C)
-    #define make_FloatingPoint(x, y) make_magmaFloatComplex(x, y);
+    #define make_FloatingPoint(x, y) make_cuFloatComplex(x, y);
   #endif
 #else
     #define conj(A)           (A)

magmablas/zgemvt_fermi.cu
@@ -28,7 +28,7 @@ zgemvtn_kernel2_fermi(int n, int m, int n1, magmaDoubleComplex alpha,  magmaDoub
   for(int i=0; i<n1; i += num_threads ){
     __syncthreads();
     //buff[threadIdx.x]  = x[i];
-    buff[threadIdx.x]  = make_magmaDoubleComplex (cuCreal(x[i*incx]), -cuCimag(x[i*incx]));
+    buff[threadIdx.x]  = MAGMA_Z_MAKE(cuCreal(x[i*incx]), -cuCimag(x[i*incx]));
 
     __syncthreads();
     #pragma unroll
@@ -41,7 +41,7 @@ zgemvtn_kernel2_fermi(int n, int m, int n1, magmaDoubleComplex alpha,  magmaDoub
 
   if (m>n1){
      //buff[threadIdx.x]  = x[n1];
-     buff[threadIdx.x]  = make_magmaDoubleComplex (cuCreal(x[n1*incx]), -cuCimag(x[n1*incx]));
+     buff[threadIdx.x]  = MAGMA_Z_MAKE(cuCreal(x[n1*incx]), -cuCimag(x[n1*incx]));
 
      __syncthreads();
      for(int j=0; j<(m-n1); j++){